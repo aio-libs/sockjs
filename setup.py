61528551847dc208fbd1e187fb5c91e27d89373a
Nikolay Kim <fafhrd91@gmail.com>
2015-04-08 16:34:32 -0700

    rename package

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(f):
     return open(os.path.join(os.path.dirname(__file__), f)).read().strip()
 
 
-setup(name='aiosockjs',
+setup(name='sockjs',
       version=version,
       description=('SockJS server implementation for aiohttp.'),
       long_description='\n\n'.join((read('README.rst'), read('CHANGES.txt'))),
@@ -21,9 +21,7 @@ setup(name='aiosockjs',
           "Programming Language :: Python",
           "Programming Language :: Python :: 3.3",
           "Programming Language :: Python :: 3.4",
-          "Programming Language :: Python :: 3.5",
           "Programming Language :: Python :: Implementation :: CPython",
-          "Framework :: aiohttp",
           "Topic :: Internet :: WWW/HTTP"],
       author='Nikolay Kim',
       author_email='fafhrd91@gmail.com',
