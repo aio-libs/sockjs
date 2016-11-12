cebaa24196c61cc1b56150dac613aabd4f521f79
Andrew Svetlov <andrew.svetlov@gmail.com>
2016-11-12 17:21:24 +0200

    Fix tests

--- tests/test_protocol.py
+++ tests/test_protocol.py
@@ -1,33 +1,30 @@
 import json
-import unittest
 
 from sockjs import protocol
 
 
-class TestProtocol(unittest.TestCase):
+def test_encode():
+    assert protocol.dumps({}) == json.dumps({})
+    assert protocol.dumps(['test']) == json.dumps(['test'])
+    assert protocol.dumps('"test"') == json.dumps('"test"')
 
-    def test_encode(self):
-        self.assertEqual(protocol.dumps({}), json.dumps({}))
-        self.assertEqual(
-            protocol.dumps(['test']), json.dumps(['test']))
-        self.assertEqual(
-            protocol.dumps('"test"'), json.dumps('"test"'))
 
-    def test_decode(self):
-        self.assertEqual(protocol.loads(json.dumps({})), {})
-        self.assertEqual(protocol.loads(json.dumps(['test'])), ['test'])
-        self.assertEqual(protocol.loads(json.dumps('"test"')), '"test"')
+def test_decode():
+    assert protocol.loads(json.dumps({})) == {}
+    assert protocol.loads(json.dumps(['test'])) == ['test']
+    assert protocol.loads(json.dumps('"test"')) == '"test"'
 
-    def test_close_frame(self):
-        msg = protocol.close_frame(1000, 'Internal error')
-        self.assertEqual(msg, 'c[1000,"Internal error"]')
 
-    def test_message_frame(self):
-        msg = protocol.message_frame('msg1')
-        self.assertEqual(
-            msg, 'a%s' % protocol.dumps(['msg1']))
+def test_close_frame():
+    msg = protocol.close_frame(1000, 'Internal error')
+    assert msg == 'c[1000,"Internal error"]'
 
-    def test_messages_frame(self):
-        msg = protocol.messages_frame(['msg1', 'msg2'])
-        self.assertEqual(
-            msg, 'a%s' % protocol.dumps(['msg1', 'msg2']))
+
+def test_message_frame():
+    msg = protocol.message_frame('msg1')
+    assert msg == 'a%s' % protocol.dumps(['msg1'])
+
+
+def test_messages_frame():
+    msg = protocol.messages_frame(['msg1', 'msg2'])
+    assert msg == 'a%s' % protocol.dumps(['msg1', 'msg2'])
