61a0528ee3116dffc9ff27b62a7e84a4758dc037
Andrew Svetlov <andrew.svetlov@gmail.com>
2018-02-25 12:52:55 +0300

    Bump to 0.7.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with codecs.open(os.path.join(os.path.abspath(os.path.dirname(
     except IndexError:
         raise RuntimeError('Unable to determine version.')
 
-install_requires = ['aiohttp >= 1.3.3']
+install_requires = ['aiohttp >= 3.0.0']
 
 
 def read(f):
