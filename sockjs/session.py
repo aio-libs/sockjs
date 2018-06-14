8e8b2ba6b17ee68be028ae690b7cacc872ca491c
joao.alves <joao.alves@smarkets.com>
2018-06-14 10:26:19 +0100

    Change default value back to 5

--- sockjs/session.py
+++ sockjs/session.py
@@ -255,7 +255,7 @@ class SessionManager(dict):
     _hb_task = None  # gc task
 
     def __init__(self, name, app, handler, loop,
-                 heartbeat=25.0, timeout=timedelta(seconds=40), debug=False):
+                 heartbeat=25.0, timeout=timedelta(seconds=5), debug=False):
         self.name = name
         self.route_name = 'sockjs-url-%s' % name
         self.app = app
